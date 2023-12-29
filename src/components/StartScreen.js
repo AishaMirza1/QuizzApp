312f3332b986f3ab58096a6c1862a7918667c726
Home <aishamirza0002@gmail.com>
2023-12-29 20:41:37 +0530

    fix: question are now in app(api not working for now a temporary fix)

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,10 +1,19 @@
-export default function StartScreen({numQuestions,dispatch}){
-    return(
-        <div className="start">
-            <h2>Welcome to the React Quizz</h2>
-            <h3>{numQuestions} questions to test your React mastery</h3>
-            <button className="btn btn-ui" onClick={()=>dispatch({type:"start"})}>Let's Start</button>
+export default function StartScreen({ numQuestions, dispatch, status }) {
+  return (
+    <div className="start">
+      <h2>Welcome to the React Quizz</h2>
+      {status !== "active" && (
+        <>
+          <h3>{numQuestions} questions to test your React mastery</h3>
 
-        </div>
-    )
+          <button
+            className="btn btn-ui"
+            onClick={() => dispatch({ type: "start" })}
+          >
+            Let's Start
+          </button>
+        </>
+      )}
+    </div>
+  );
 }
